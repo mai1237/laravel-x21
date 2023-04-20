18f36487facc4ac6293bd47636ef3f8af202c61c
mai1237 <periklis.kiorpes@gmail.com>
2023-04-20 10:35:45 +0300

    step 1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Profile\AvatarController;
 
 
 Route::get('/', function () {
@@ -16,6 +17,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::patch('/profile/avatar', [AvatarController::class, 'update'])->name('profile.avatar');
+    Route::post('/profile/avatar/ai', [AvatarController::class, 'generate'])->name('profile.avatar.ai');
 });
 
 require __DIR__.'/auth.php';
+
+
